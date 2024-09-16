829b87050f3cd3378e05d19f6103a8333f5bbcfb
macn1 <athulraihan27@gmail.com>
2024-09-16 23:32:08 +0530

    jaipur

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Navbar from '../../Screen/Navbar'
 import LandingHistory from './LandingHistory'
+import Jaipur from './Jaipur'
 
 function History() {
   return (
     <div>
         <Navbar></Navbar>
         <LandingHistory/>
+        <Jaipur/>
       
     </div>
   )
---
+++ src/components/History/Jaipur.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './jaipur.css'
+
+import img1 from '../../folder/jaipur.jpg'
+
+function Jaipur() {
+  return (
+    <div>
+      
+      <div className='jaipur w-100'>
+        <img src={img1} alt="" style={{ width: '100%' }}/>
+      
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Jaipur
