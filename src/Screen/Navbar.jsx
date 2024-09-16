5079111b147508a846b87f72e260b662091f806c
macn1 <athulraihan27@gmail.com>
2024-09-16 20:09:27 +0530

    navbvar

--- src/Screen/Navbar.jsx
+++ src/Screen/Navbar.jsx
@@ -3,12 +3,12 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-black" style={{height:'20%'}}>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black" style={{height:'90px'}}>
       <div className="container">
  
-        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-        </button> */}
+        </button>
         <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
           <ul className="navbar-nav mx-auto w-100 d-flex justify-content-between">
             <li className="nav-item">
