38c782b8f3adb714f07e72322577275d78775e40
macn1 <athulraihan27@gmail.com>
2024-09-16 21:06:17 +0530

    navbvar1

--- src/components/History/LandingHistory.jsx
+++ src/components/History/LandingHistory.jsx
@@ -5,13 +5,14 @@ import img1 from '../../folder/top.jpg'
 import './historyland.css'
 
 import img2 from '../../folder/1.jpg'
-import img3 from '../../folder/1.jpg'
-import img4 from '../../folder/1.jpg'
-import img5 from '../../folder/1.jpg'
-import img6 from '../../folder/1.jpg'
-import img7 from '../../folder/1.jpg'
-import img8 from '../../folder/1.jpg'
-import img9 from '../../folder/1.jpg'
+import img3 from '../../folder/2.jpg'
+import img4 from '../../folder/3.jpg'
+import img5 from '../../folder/4.jpg'
+import img6 from '../../folder/5.jpg'
+import img7 from '../../folder/6.jpg'
+import img8 from '../../folder/7.jpg'
+import img9 from '../../folder/8.jpg'
+import img10 from '../../folder/9.jpg'
 
 
 
@@ -24,31 +25,49 @@ function LandingHistory() {
                 <div className='text-white text-center'>
                     <h2>The Royal Legacy Behind Jantar Mantar, Jaipur</h2>
 
-                    <div className='container ' style={{ backgroundColor: 'red' }}>
+                    <div className='container ' >
 
                         <div className='row'>
 
                             <div className='col-12'>
-                                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                                     <div class="carousel-inner">
                                         <div class="carousel-item active">
-                                            <img class="d-block w-100" src={img2} alt="First slide" />
+                                            <img src={img2} class="d-block w-100" alt="..." />
                                         </div>
                                         <div class="carousel-item">
-                                            <img class="d-block w-100" src={img3} alt="Second slide" />
+                                            <img src={img3} class="d-block w-100" alt="..." />
                                         </div>
                                         <div class="carousel-item">
-                                            <img class="d-block w-100" src={img4} alt="Third slide" />
+                                            <img src={img4} class="d-block w-100" alt="..." />
+                                        </div>
+                                        <div class="carousel-item ">
+                                            <img src={img5} class="d-block w-100" alt="..." />
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src={img6} class="d-block w-100" alt="..." />
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src={img7} class="d-block w-100" alt="..." />
+                                        </div>
+                                        <div class="carousel-item ">
+                                            <img src={img8} class="d-block w-100" alt="..." />
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src={img9} class="d-block w-100" alt="..." />
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src={img10} class="d-block w-100" alt="..." />
                                         </div>
                                     </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
                                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
                                 </div>
 
                             </div>
