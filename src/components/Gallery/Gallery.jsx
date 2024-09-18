61ca49eea616c12eddc904d571fe56f05fd3b2cb
macn1 <athulraihan27@gmail.com>
2024-09-18 21:33:33 +0530

    gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../Screen/Navbar'
+import Galleryh from './Galleryh'
+
+
 
 function Gallery() {
   return (
     <div>
         <Navbar></Navbar>
+        <Galleryh></Galleryh>
       
     </div>
   )
---
+++ src/components/Gallery/Galleryh.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+import './gallery.css'
+
+function Galleryh() {
+  return (
+    <div>
+      <div className='gall'>
+
+        <div className='text-center text-white pt-5'>
+            <h2> Discover Jantar Mantar Through Pictures</h2>
+
+            <div className='container'>
+            <p className='pt-3'>
+            Step into the world of Jantar Mantar through a captivating collection of images that showcase the grandeur and precision of this ancient astronomical observatory. Each photograph tells a story of how science, architecture, and heritage converge in the form of massive stone instruments that have stood the test of time.
+
+Whether it’s the towering Samrat Yantra or the intricately designed Jai Prakash Yantra, these images capture the scale, detail, and significance of the instruments, offering a visual journey through centuries of astronomical brilliance. Take a closer look and be inspired by Jaipur’s celestial wonders.
+
+            </p>
+
+
+            </div>
+         
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Galleryh
